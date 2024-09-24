2ae82af523f9346d4df5651063e587a3bc73ca1e
malikqattoum <malikqattom@gmail.com>
2024-09-24 21:22:43 +0300

    Remove our_mission_food_education_medicine

--- resources/views/front_landing/campaigns.blade.php
+++ resources/views/front_landing/campaigns.blade.php
@@ -12,7 +12,7 @@
                     <div class="row ">
                         <div class="col-lg-6 col-md-7 parallelogram-shape">
                             <div class="text-responsive inner-text position-relative">
-                                <p class="fs-18 fw-5 mb-md-3 pb-lg-2 mb-2">{{__('messages.front_landing.our_mission_food_education_medicine')}}</p>
+                                {{-- <p class="fs-18 fw-5 mb-md-3 pb-lg-2 mb-2">{{__('messages.front_landing.our_mission_food_education_medicine')}}</p> --}}
                                 <h2 class="fs-1 mb-md-0 fw-6">{{__('messages.front_landing.our_causes')}}</h2>
                             </div>
                         </div>
